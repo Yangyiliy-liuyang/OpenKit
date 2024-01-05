3bf599212413a15a6604b7d53b009417f2dd86cd
yangyiliy <yangyiliy@foxmail.com>
2024-01-05 16:11:15 +0800

    [update]项目结构布局修改

--- slice.go
+++ slice.go
@@ -1,5 +1,43 @@
 package main
 
+// Find 指定过滤器查找
+func Find[T any](vals []T, filter func(t T) bool) T {
+	for _, v := range vals {
+		if filter(v) {
+			return v
+		}
+	}
+	var t T
+	return t
+}
+
+// FindAll 指定过滤器查找所有
+func FindAll[T any](vals []T, filter func(t T) bool) []T {
+	var result []T
+	for _, v := range vals {
+		if filter(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// Insert 指定下标位置插入
+func Insert[T any](idx int, val T, vals []T) []T {
+	if idx < 0 || idx > len(vals) {
+		panic("idx不合法")
+	}
+	//扩容
+	vals = append(vals, val)
+	for i := len(vals) - 1; i > idx; i-- {
+		if i-1 > 0 {
+			vals[i] = vals[i-1]
+		}
+	}
+	vals[idx] = val
+	return vals
+}
+
 /*
 作业：实现切片的删除操作
 实现删除切片特定下标元素的方法。
