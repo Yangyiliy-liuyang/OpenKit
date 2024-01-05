a46f5d9a259369467a5f204a8454fab91f8cf450
yangyiliy <yangyiliy@foxmail.com>
2024-01-05 17:45:39 +0800

    [update]Delete 指定下标位置删除,返回新切片数组、被删除元素、错误

--- slice.go
+++ slice.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 // Find 指定过滤器查找
 func Find[T any](vals []T, filter func(t T) bool) T {
 	for _, v := range vals {
@@ -23,7 +25,7 @@ func FindAll[T any](vals []T, filter func(t T) bool) []T {
 }
 
 // Insert 指定下标位置插入
-func Insert[T any](idx int, val T, vals []T) []T {
+func Insert[T any](vals []T, idx int, val T) []T {
 	if idx < 0 || idx > len(vals) {
 		panic("idx不合法")
 	}
@@ -47,11 +49,22 @@ func Insert[T any](idx int, val T, vals []T) []T {
 要求三：改造为泛型方法
 要求四：支持缩容，并旦设计缩容机制。
 */
-func DelectV1(src []int, index int) []int {
-	return []int{}
+
+// Delete 指定下标位置删除,返回新切片数组、被删除元素、错误。
+func Delete[T any](src []T, index int) ([]T, T, error) {
+	length := len(src)
+	if index < 0 || index > length {
+		var t T
+		//return nil, t, ErrIndexOutOfRange(length, index)
+		return nil, t, errors.New("index不合法")
+	}
+	//src[index+1:] 表示从切片 src 的索引 index+1 开始到末尾的所有元素组成的切片。
+	//然后使用 ... 将这个切片展开，作为函数 append 的参数,实现逐个添加到 append 函数的参数列表中
+	res := src[index]
+	src = append(src[:index], src[index+1:]...)
+	return src, res, nil
 }
 
-func Delete[Src any](src []Src, index int) []Src {
-	src = append(src[:index], src[index+1:]...)
-	return src
+func DelectV1(src []int, index int) []int {
+	return []int{}
 }
