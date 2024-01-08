53ef2b99226c9f09cd52e9ac754779cc104e786a
yangyiliy <yangyiliy@foxmail.com>
2024-01-08 23:15:27 +0800

    [fix]格式化修正

--- list.go
+++ list.go
@@ -5,9 +5,11 @@ type List[T any] interface {
 	Append(val T) error
 	Delete(index int) error
 }
+
 type LinkedList[T any] struct {
 	head *node[T]
 }
+
 type node[T any] struct {
 	data T
 }
@@ -19,5 +21,4 @@ func (l *LinkedList[T]) Add(index int, val T) {
 func UseList() {
 	l := &LinkedList[int]{}
 	l.Add(1, 123)
-
 }
