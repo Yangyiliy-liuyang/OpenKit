4d8f5a1c0d74db91a2821b8f0d609b37bbbb20fb
yangyiliy <yangyiliy@foxmail.com>
2024-04-09 01:51:26 +0800

    [fix] masked 序列化时加密phone字段

--- gox/masked_test.go
+++ gox/masked_test.go
@@ -9,6 +9,18 @@ import (
 type User struct {
 	Email    string
 	Password MaskedSting
+	Phone    MaskedPhone
+}
+
+type MaskedPhone string
+
+// 序列化时加密字段
+func (m MaskedPhone) MarshalJSON() ([]byte, error) {
+	str := string(m)
+	if len(str) == 11 {
+		str = str[0:3] + "****" + str[7:]
+	}
+	return []byte(`"` + str + `"`), nil
 }
 
 type MaskedSting string
@@ -23,10 +35,12 @@ func TestMasked(t *testing.T) {
 	u := User{
 		Email:    "envkt@example.com",
 		Password: "123456",
+		Phone:    "13812345678",
 	}
 	val, err := json.Marshal(u)
 	assert.NoError(t, err)
 	t.Log(string(val))
 	// {"Email":"envkt@example.com","Password":{}}
 	// {"Email":"envkt@example.com","Password":"****"}
+	// {"Email":"envkt@example.com","Password":"****","Phone":"138****5678"}
 }
