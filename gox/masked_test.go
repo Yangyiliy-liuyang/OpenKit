b533da6ab2805bd3bbaf5c1e1206251073ff7007
yangyiliy <yangyiliy@foxmail.com>
2024-04-09 01:27:30 +0800

    [update] MaskedSting 序列化时加密字段

---
+++ gox/masked_test.go
@@ -0,0 +1,29 @@
+package gox
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type User struct {
+	Email    string
+	Password MaskedSting
+}
+
+type MaskedSting string
+
+// 序列化时加密字段
+func (m MaskedSting) MarshalJSON() ([]byte, error) {
+	return []byte("{}"), nil
+}
+
+func TestMasked(t *testing.T) {
+	u := User{
+		Email:    "envkt@example.com",
+		Password: "123456",
+	}
+	val, err := json.Marshal(u)
+	assert.NoError(t, err)
+	t.Log(string(val)) // {"Email":"envkt@example.com","Password":{}}
+}
