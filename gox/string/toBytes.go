90834b29fd702761c8f6f9a06699e545a655471a
yangyiliy <yangyiliy@foxmail.com>
2024-04-08 15:17:32 +0800

    [update] StringToBytes

---
+++ gox/string/toBytes.go
@@ -0,0 +1,13 @@
+package string
+
+import "unsafe"
+
+// StringToBytes  string to []byte
+func StringToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
